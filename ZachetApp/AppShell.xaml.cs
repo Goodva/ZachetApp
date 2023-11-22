07159882b9193f15be26edf24b003e74cbf37f03
Ivan Zhilyackov <zhilyackov.vanya@gmail.com>
2023-11-22 14:08:51 +0700

    Добавьте файлы проекта.

---
+++ ZachetApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ZachetApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ZachetApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ZachetApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
